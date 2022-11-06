a412eb4a14761e88f69fec4d7d91ecaf96bc1ea0
Abdullah97sh <abdallahs.hussein@gmail.com>
2022-11-06 05:49:28 +0300

    fix error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import React from "react";@push ('name') 
-  
-@endpush
+import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 import reportWebVitals from "./reportWebVitals";
